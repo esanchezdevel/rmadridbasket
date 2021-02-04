c0e737091bf79a9a509ac6b00b6647fd1f0207a6
Enrique Sanchez <enrique.sanchez@docomodigital.com>
2021-02-04 15:51:39 +0100

    first commit

---
+++ src/main/java/es/basket/rmadrid/RmadridbasketApplication.java
@@ -0,0 +1,13 @@
+package es.basket.rmadrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmadridbasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmadridbasketApplication.class, args);
+	}
+
+}
