07ab71cc0a5f39275fadb732c4e68d0f07deb937
develtricks <develtricks@gmail.com>
2021-02-13 13:12:37 +0100

    docu - update database script

--- src/main/resources/docu/database.sql
+++ src/main/resources/docu/database.sql
@@ -4,20 +4,22 @@
 
 CREATE DATABASE rmadridbasket;
 
+USE rmadridbasket;
+
 /*SITE_CONFIGURATION*/
 
 CREATE TABLE site_configuration (
 	id int PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(100) NOT NULL,
 	value LONGTEXT NOT NULL,
-	updated TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP
+	updated TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO site_configuration (name, value) VALUES ('title', 'rmadridbasket.es');
-INSERT INTO site_configuration (name, value) VALUES ('description', 'Toda la actualidad sobre el Real Madrid de Baloncesto');
-INSERT INTO site_configuration (name, value) VALUES ('keywords', 'real madrid, baloncesto, madrid, real, basket, basketball, acb, euroleague, euroliga, liga, endesa');
-INSERT INTO site_configuration (name, value) VALUES ('author', 'rmadridbasket.es');
-INSERT INTO site_configuration (name, value) VALUES ('copyright', 'rmadridbasket.es');
+INSERT INTO site_configuration (name, `value`) VALUES ('site-title', 'RMadrid Basket');
+INSERT INTO site_configuration (name, `value`) VALUES ('site-description', 'Toda la actualidad sobre el Real Madrid de Baloncesto');
+INSERT INTO site_configuration (name, `value`) VALUES ('site-keywords', 'real madrid, baloncesto, madrid, real, basket, basketball, acb, euroleague, euroliga, liga, endesa');
+INSERT INTO site_configuration (name, `value`) VALUES ('site-author', 'rmadridbasket.es');
+INSERT INTO site_configuration (name, `value`) VALUES ('site-copyright', 'rmadridbasket.es');
 
 
 /*MENUS*/
